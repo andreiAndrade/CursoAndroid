1a081c52b65705d7289ff6e8816657d86768e89b
TOTVS Android <TOTVS Android>
2016-12-21 21:12:22 -0200

    Add tablet's activity layout

--- app/src/main/java/com/totvs/classificados/activity/DetailActivity.java
+++ app/src/main/java/com/totvs/classificados/activity/DetailActivity.java
@@ -2,6 +2,7 @@ package com.totvs.classificados.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.totvs.classificados.R;
@@ -14,6 +15,8 @@ import com.totvs.classificados.model.AdItem;
 public class DetailActivity extends BaseActivity {
 
     private TextView mTvTitle;
+    private TextView mTvDetail;
+    private ImageView mIvImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,17 @@ public class DetailActivity extends BaseActivity {
 
         setContentView(R.layout.activity_detail);
         mTvTitle = (TextView) findViewById(R.id.tv_title);
+        mTvDetail = (TextView) findViewById(R.id.tv_detail);
+        mIvImage = (ImageView) findViewById(R.id.iv_image);
+
 
         Intent intent = getIntent();
         if (intent != null) {
             AdItem item = (AdItem) intent.getSerializableExtra("AD_KEY");
-            mTvTitle.setText(item.getDetail());
+
+            mTvTitle.setText(item.getTitle());
+            mTvDetail.setText(item.getDetail());
+            mIvImage.setImageResource(R.mipmap.ic_launcher);
         }
     }
 }
